c4c41bec53e8d4ddb7b2bb73c7ee24e0a1ddb5b1
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-01-07 12:48:11 +0530

    Implemented readline input and output

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log(`Hello World...`);
+import * as readline from 'node:readline/promises'
+
+const readlineObj: readline.Interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+try {
+    const userInp: string = await readlineObj.question('Please type a word: ', {
+        signal: AbortSignal.timeout(10_000)
+    });
+
+} finally {
+    readlineObj.close();
+}
